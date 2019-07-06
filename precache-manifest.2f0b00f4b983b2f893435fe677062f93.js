ea3314014766d9680cbcf47309f78d511a5fa23e
Philipp Mossier <p.mossier.development@gmail.com>
2019-07-06 19:00:08 +0200

    Updates

---
+++ precache-manifest.2f0b00f4b983b2f893435fe677062f93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4108fae9d1b9d27ea6f8e1fc1b1e6b8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "fd8a62793abb62830034",
+    "url": "/monsters-rolodex/static/css/main.d648e8e5.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "fd8a62793abb62830034",
+    "url": "/monsters-rolodex/static/js/main.1847d05a.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
